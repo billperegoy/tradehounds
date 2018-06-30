b1d552c2c16a14f608e61dea9e45903016f009fa
Bill Peregoy <bill@gnar.dog>
2018-06-30 13:24:00 -0400

    Cleaned up code coverage settings to skip test dir

--- test/tradehounds_web/controllers/healthcheck_controller_test.exs
+++ test/tradehounds_web/controllers/healthcheck_controller_test.exs
@@ -1,15 +1,6 @@
 defmodule TradehoundsWeb.HealthcheckControllerTest do
   use TradehoundsWeb.ConnCase
 
-  alias Tradehounds.Admin
-
-  @create_attrs %{}
-
-  def fixture(:healthcheck) do
-    {:ok, healthcheck} = Admin.create_healthcheck(@create_attrs)
-    healthcheck
-  end
-
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
   end
@@ -21,7 +12,7 @@ defmodule TradehoundsWeb.HealthcheckControllerTest do
         |> get(healthcheck_path(conn, :index))
         |> doc(description: "Perform healthceck", operation_id: "list_healthchecks")
 
-      assert json_response(conn, 200)["status"] == "ok"
+      assert json_response(conn, 200) == %{"status" => "ok"}
     end
   end
 end
